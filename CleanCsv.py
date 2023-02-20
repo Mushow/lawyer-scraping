ce9d96bf1db3617c9c4ddf19e06235482cb1ac59
Mushow <ajames@guardiaschool.fr>
2023-02-20 19:20:18 +0100

    Sort and clean values

--- CleanCsv.py
+++ CleanCsv.py
@@ -1,25 +1,26 @@
 import pandas as pd
 
-file = 'lawyers.csv'
-data = pd.read_csv(file)
+
+def readCsv(file):
+    return pd.read_csv(file)
 
 
-def checkDupes(column):
-    mask = data.loc[data[column].duplicated(keep=False), :]
-    data.loc[mask, column] = ""
+def checkDupes(df, column):
+    mask = df.loc[df[column].duplicated(keep=False), :]
+    df.loc[mask, column] = ""
 
 
-def toNumeric(column):
-    data[column] = pd.to_numeric(data[column], errors='coerce')
+def toNumeric(df, column):
+    df[column] = pd.to_numeric(df[column], errors='coerce')
 
 
-def nullToMean(column):
-    data.loc[data[column].isnull(), column] = data[column].mean()
+def nullToMean(df, column):
+    df.loc[df[column].isnull(), column] = df[column].mean()
 
 
-def defaultDateTime(column):
-    data[column] = pd.to_datetime(data[column], format='%d/%m/%Y', errors='coerce')
+def defaultDateTime(df, column):
+    df[column] = pd.to_datetime(df[column], format='%d/%m/%Y', errors='coerce')
 
 
-def toCsv(data_frame, name):
-    data_frame.to_csv(name, index=False)
+def toCsv(df, name):
+    df.to_csv(name, index=False)
