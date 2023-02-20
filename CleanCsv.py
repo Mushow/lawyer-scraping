7db6c8a3bc248b91dc3aecef6b3ba076c82759b1
Mushow <ajames@guardiaschool.fr>
2023-02-20 19:21:51 +0100

    Sort and clean values

--- CleanCsv.py
+++ CleanCsv.py
@@ -8,19 +8,24 @@ def readCsv(file):
 def checkDupes(df, column):
     mask = df.loc[df[column].duplicated(keep=False), :]
     df.loc[mask, column] = ""
+    return df
 
 
 def toNumeric(df, column):
     df[column] = pd.to_numeric(df[column], errors='coerce')
+    return df
 
 
 def nullToMean(df, column):
     df.loc[df[column].isnull(), column] = df[column].mean()
+    return df
 
 
 def defaultDateTime(df, column):
     df[column] = pd.to_datetime(df[column], format='%d/%m/%Y', errors='coerce')
+    return df
 
 
 def toCsv(df, name):
     df.to_csv(name, index=False)
+    return df
