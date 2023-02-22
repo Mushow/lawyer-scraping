b3b804e3ce76153085f0a59311d0220667fad010
MushowUK <uk.antoine.james@gmail.com>
2023-02-22 12:50:17 +0100

    Implemented csvUtils methods

--- Lawyer.py
+++ Lawyer.py
@@ -1,11 +1,32 @@
 class Lawyer:
     def __init__(self, lawyer_name, lawyer_number, lawyer_email, cases, sworn_date, address, city):
-        self.lawyer_name = lawyer_name
-        self.lawyer_number = lawyer_number
-        self.lawyer_email = lawyer_email
+        self.set_name(lawyer_name)
+        self.set_number(lawyer_number)
+        self.set_email(lawyer_email)
+        self.set_cases(cases)
+        self.set_sworn_date(sworn_date)
+        self.set_address(address)
+        self.set_city(city)
+
+    def set_name(self, name):
+        self.lawyer_name = name
+
+    def set_number(self, number):
+        self.lawyer_number = number
+
+    def set_email(self, email):
+        self.lawyer_email = email
+
+    def set_cases(self, cases):
         self.cases = cases
+
+    def set_sworn_date(self, sworn_date):
         self.sworn_date = sworn_date
+
+    def set_address(self, address):
         self.address = address
+
+    def set_city(self, city):
         self.city = city
 
     def get_name(self):
