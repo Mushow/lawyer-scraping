2928cc43a8ce034587fd835e520a1950a4987296
james <antoine@mushow.uk>
2023-02-23 09:56:48 +0100

    [BC] Correctly format the website url in CSV

--- lawyerScraping.py
+++ lawyerScraping.py
@@ -1,147 +1,91 @@
-import asyncio
 import re
-import time
 
-import aiohttp
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
-import csvUtils
+import messages
 from Lawyer import Lawyer
 
-
-async def get_links(session, url):
-    async with session.get(url) as response:
-        soup = BeautifulSoup(await response.text(), 'lxml')
-        cards = soup.find_all('div', class_='entry-content')
-        return [card.find('a').get('href') for card in cards]
+base_url = "https://www.barreaulyon.com"
+uri = "/annuaire?paged="
+final_url = base_url + uri
+total_pages = 15
 
 
-def get_clean_text(text):
-    return re.sub(' +', ' ', text.get_text(strip=True))
+def swoup(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.text, 'lxml') if response.ok else messages.response_error() and None
 
 
-def to_date(sworn_date):
-    months = {
-        "janvier": 1,
-        "février": 2,
-        "mars": 3,
-        "avril": 4,
-        "mai": 5,
-        "juin": 6,
-        "juillet": 7,
-        "août": 8,
-        "septembre": 9,
-        "octobre": 10,
-        "novembre": 11,
-        "décembre": 12
-    }
-
-    day, month_name, year = sworn_date.split()
-    month = months[month_name]
-    date = f"{day}/{month:02d}/{year}"
-
-    return date
+def get_links():
+    return [final_url + str(page) for page in range(1, total_pages + 1)]
 
 
-async def get_lawyer(session, link):
-    async with session.get(link) as response:
-        soup = BeautifulSoup(await response.text(), 'lxml')
-        name_element = soup.find('header', class_='entry-header')
-        number_element = soup.find('div', class_='entry-infos__item--tel')
-        email_element = soup.find('div', class_='entry-infos__item--mail')
-        subsequent_information = soup.findAll('div', class_='entry-content__item')
+def get_page_from_url(page):
+    return page.split("=")[1]
 
-        try:
-            name = get_clean_text(name_element.find('h1'))
-            number = get_clean_text(number_element.find('a'))
-            email = get_clean_text(email_element.find('a'))
-            cases = sworn_date = address = postal_code = None
-            for info in subsequent_information:
-                key = info.find('b').text
-                match key:
-                    case "Case":
-                        cases = get_clean_text(info.find('p'))
-                    case "Prestation de serment":
-                        sworn_date = get_clean_text(info.find('p'))
-                    case "Rue":
-                        address = get_clean_text(info.find('p')).replace(" ", " ")
-                    case "Code postal":
-                        postal_code = get_clean_text(info.find('p')).upper()
 
-            if cases or sworn_date or address or postal_code is None:
-                pass
+def success_soup(page):
+    messages.success_links()
+    messages.page_success(get_page_from_url(page))
 
-            return Lawyer(name, number, email, cases, to_date(sworn_date), address, postal_code)
-        except AttributeError:
-            pass
+
+def find_lawyer_cards(soup):
+    return soup.findAll("div", class_="entry-content")
+
+
+def clean_text(text):
+    return re.sub(' +', ' ', text.get_text(strip=True)).replace(" ", " ").replace("\n", "").replace(" - -", "")
+
+
+def get_info(card, class_name):
+    try:
+        return clean_text(card.find("p", class_=class_name))
+    except (AttributeError, TypeError):
+        return ""
+
+
+def get_card_info(card):
+    class_names = [
+        "entry-title",
+        "card-annuaire__info--tel",
+        "card-annuaire__info--mail",
+        "card-annuaire__info--adress",
+        "card-annuaire__info--website",
+    ]
+
+    info_values = (get_info(card, class_name) for class_name in class_names)
+    return Lawyer(*info_values)
 
 
 def format_lawyers(lawyers_array):
     lawyers_data = []
     for lawyer in lawyers_array:
         if lawyer:
+            website = lawyer.get_website()
             lawyer_dict = {
                 'Name': lawyer.get_name(),
-                'Phone': lawyer.get_number(),
-                'Cases': lawyer.get_cases(),
+                'Number': lawyer.get_number(),
                 'Email': lawyer.get_email(),
                 'Address': lawyer.get_address(),
-                'Sworn Date': lawyer.get_sworn_date(),
-                'City': lawyer.get_city()
+                'Website': "https://" + website if website and not website.startswith("https://") else website
             }
             lawyers_data.append(lawyer_dict)
 
     return lawyers_data
 
 
-def sort_df(df, value):
-    df = df.sort_values(value, ascending=True)
-    return df
+def main():
+    lawyers = []
+    for page in get_links():
+        soup = swoup(page)
+        if soup:
+            success_soup(page)
+            lawyers.extend(get_card_info(card) for card in find_lawyer_cards(soup))
+    lawyers = format_lawyers(lawyers)
+    pd.DataFrame(lawyers).to_csv("lawyers.csv", index=False)
+    messages.finished_success()
 
 
-async def main():
-    baseUrl = "https://www.barreaulyon.com/"
-    uri = "annuaire?paged="
-    lastPage = 333
-
-    async with aiohttp.ClientSession() as session:
-        links_tasks = []
-        lawyers_tasks = []
-        for pageNb in range(1, lastPage + 1):
-            url = baseUrl + uri + str(pageNb)
-            links_tasks.append(get_links(session, url))
-
-        links = await asyncio.gather(*links_tasks)
-        links_array = [link for page in links for link in page]
-
-        for link in links_array:
-            lawyers_tasks.append(get_lawyer(session, link))
-
-        lawyers_array = await asyncio.gather(*lawyers_tasks)
-
-        lawyers_data = format_lawyers(lawyers_array)
-
-        df = pd.DataFrame(lawyers_data)
-        df = sort_df(df, 'Cases')
-        print(df.values.tolist())
-        df = csvUtils.nullToZero(df, 'Cases')
-
-        keywords = ['city', 'address', 'sworn date']
-
-        for col in df.columns:
-            if any(keyword in col.lower() for keyword in keywords):
-                df = csvUtils.nullToUnknown(df, col)
-
-        df = csvUtils.dropNull(df, ['Phone', 'Email'])
-
-        df.to_csv('lawyers.csv', index=False)
-
-        return lawyers_data
-
-if __name__ == '__main__':
-    tic = time.perf_counter()
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    lawyers = asyncio.run(main())
-    toc = time.perf_counter()
-    print(f"Retrieved {len(lawyers)} lawyers in {toc - tic:0.4f} seconds")
+main()
