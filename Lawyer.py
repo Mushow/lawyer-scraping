e0f928eee07083bdc8ed2d362a545b2945bdfb35
james <antoine@mushow.uk>
2023-02-23 09:28:27 +0100

    [NF] Scraping the lawyer website

--- Lawyer.py
+++ Lawyer.py
@@ -1,21 +1,29 @@
 class Lawyer:
-    def __init__(self, lawyer_name, lawyer_number, lawyer_email, address):
-        self.set_name(lawyer_name)
-        self.set_number(lawyer_number)
-        self.set_email(lawyer_email)
+    def __init__(self, name, number, email, address, website):
+        self.set_name(name)
+        self.set_number(number)
+        self.set_email(email)
         self.set_address(address)
+        self.set_website(website)
 
     def set_name(self, name):
         self.lawyer_name = name
+        return self
 
-    def set_number(self, lawyer_number):
-        self.lawyer_number = lawyer_number
+    def set_number(self, number):
+        self.lawyer_number = number
+        return self
 
-    def set_email(self, lawyer_email):
-        self.lawyer_email = lawyer_email
+    def set_email(self, email):
+        self.lawyer_email = email
+        return self
 
-    def set_address(self, lawyer_address):
-        self.address = lawyer_address
+    def set_address(self, address):
+        self.address = address
+        return self
+
+    def set_website(self, website):
+        self.website = website
 
     def get_name(self):
         return self.lawyer_name
@@ -28,3 +36,6 @@ class Lawyer:
 
     def get_address(self):
         return self.address
+
+    def get_website(self):
+        return self.website
